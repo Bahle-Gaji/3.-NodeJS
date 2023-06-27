04d6c8251a85833c45dd20d83c393278ca38ae3b
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-06-27 23:51:26 +0200

    Use Summernote Textbox and Link to Home Page

--- main_project/controllers/newPost.js
+++ main_project/controllers/newPost.js
@@ -2,7 +2,9 @@
 
 module.exports = (req, res) => {
     if(req.session.userId){
-        return res.render('create')
+        return res.render('create', {
+            createPost: true
+        })
     }else {
         res.redirect('/auth/login')
     }
