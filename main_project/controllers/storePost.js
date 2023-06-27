36ed718d42838f0ea9106f6fe7343d990ebf6046
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-06-27 18:18:06 +0200

    Modify BlogPost Img DirName

--- main_project/controllers/storePost.js
+++ main_project/controllers/storePost.js
@@ -5,12 +5,12 @@ const BlogPost = require('../models/BlogPost'),
 
 module.exports = (req, res) => {
     let image = req.files.image;
-    image.mv(path.resolve(__dirname, 'public/assets/img', image.name), async (error) => {
+    image.mv(path.resolve(__dirname, '../public/assets/img', image.name), async (error) => {
         await BlogPost.create({
             title: req.body.title,
             body: req.body.body,
             username: req.body.username,
-            image: '/img/' + image.name
+            image: '/assets/img/' + image.name
         })
         res.redirect('/')
     })
